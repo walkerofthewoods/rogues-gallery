79a7e04cb0a203fb6575e841b7fe9eaf0952fb48
Walker <walkerrm@gmail.com>
2020-11-27 13:07:28 -0500

    initial re-package and commit w proxy server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const request = require('request');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', process.env.ORIGIN || '*');
+	next();
+});
+
+app.get('/superheroproxy', (req, res) => {
+	const id = req.query['id'];
+	const url = `https://superheroapi.com/api/10114226739421973/${id}/image`;
+
+	request(url).pipe(res);
+});
+
+app.listen(port, () => console.log(`superheroapi-cors app listening on port ${port}!`));
