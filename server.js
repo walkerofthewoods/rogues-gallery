bb8c9c62eda5ceade88a546abfebcbb93dae13bd
Walker <walkerrm@gmail.com>
2020-11-27 21:15:55 -0500

    updating server to issue react build

--- server.js
+++ server.js
@@ -18,13 +18,15 @@ app.get('/superheroproxy', (req, res) => {
 	request(url).pipe(res);
 });
 
-/* 
-catch all redirect?
+if (process.env.NODE_ENV === 'production') {
+	// Exprees will serve up production assets
+	app.use(express.static('client/build'));
 
-app.get('*', function(request, response) {
-	response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-}); 
-
-*/
+	// Express serve up index.html file if it doesn't recognize route
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 app.listen(port, () => console.log(`superheroapi-cors app listening on port ${port}!`));
